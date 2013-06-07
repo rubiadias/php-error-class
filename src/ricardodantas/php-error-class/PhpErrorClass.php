7b31898e2d32b3686f9d686c709d8759a6765b35
Ricardo Dantas <ricardodantas@gmail.com>
2013-06-07 19:43:56 -0300

    / ‘src/ricardodantas/php-error-class/PhpErrorClass.php’

--- src/ricardodantas/php-error-class/PhpErrorClass.php
+++ src/ricardodantas/php-error-class/PhpErrorClass.php
@@ -7,6 +7,18 @@ class PhpErrorClass{
     protected static $old_error_reporting = null;
     protected static $old_display_error = null;
     protected static $send_to = 'email';
+    protected static $email_configs = array(
+                                        'smtp' => null,
+                                        'smtp_auth' => true,
+                                        'username' => null,
+                                        'password' => null,
+                                        'ssl' => true,
+                                        'tls'=>false,
+                                        'from_email' => null,
+                                        'from_name' => null,
+                                        'to_email' => null,
+                                        'subject' => '[Error Report]',
+                                    );
 
     static public function handler($code, $string, $file, $line){
         switch ($code) {
@@ -24,7 +36,7 @@ class PhpErrorClass{
         $mail = new PHPMailer;
 
         $mail->IsSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup server
+        $mail->Host = 'smtp1.example.com';                    // Specify main and backup server
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
         $mail->Username = 'jswan';                            // SMTP username
         $mail->Password = 'secret';                           // SMTP password
@@ -32,16 +44,10 @@ class PhpErrorClass{
 
         $mail->From = 'from@example.com';
         $mail->FromName = 'Mailer';
-        $mail->AddAddress('josh@example.net', 'Josh Adams');  // Add a recipient
-        $mail->AddAddress('ellen@example.com');               // Name is optional
-        $mail->AddReplyTo('info@example.com', 'Information');
-        $mail->AddCC('cc@example.com');
-        $mail->AddBCC('bcc@example.com');
 
-        $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-        $mail->AddAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        $mail->AddAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-        $mail->IsHTML(true);                                  // Set email format to HTML
+        $mail->AddAddress('ellen@example.com');               // Name is optional
+
+        $mail->IsHTML(false);                                  // Set email format to HTML
 
         $mail->Subject = 'Here is the subject';
         $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
