c7fbd56bf0af97349b85ec91329a71d6a0d2bd06
Ricardo Dantas <ricardodantas@gmail.com>
2013-06-07 19:37:45 -0300

    / ‘src/ricardodantas/php-error-class/PhpErrorClass.php’

--- src/ricardodantas/php-error-class/PhpErrorClass.php
+++ src/ricardodantas/php-error-class/PhpErrorClass.php
@@ -6,6 +6,7 @@ class PhpErrorClass{
     protected static $registered = false;
     protected static $old_error_reporting = null;
     protected static $old_display_error = null;
+    protected static $send_to = 'email';
 
     static public function handler($code, $string, $file, $line){
         switch ($code) {
@@ -17,6 +18,60 @@ class PhpErrorClass{
                 throw new \ErrorException($string, $code, $code,$file,$line);
         }
     }
+
+    protected function sendToEmail(){
+
+        $mail = new PHPMailer;
+
+        $mail->IsSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup server
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'jswan';                            // SMTP username
+        $mail->Password = 'secret';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+        $mail->From = 'from@example.com';
+        $mail->FromName = 'Mailer';
+        $mail->AddAddress('josh@example.net', 'Josh Adams');  // Add a recipient
+        $mail->AddAddress('ellen@example.com');               // Name is optional
+        $mail->AddReplyTo('info@example.com', 'Information');
+        $mail->AddCC('cc@example.com');
+        $mail->AddBCC('bcc@example.com');
+
+        $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+        $mail->AddAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        $mail->AddAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        $mail->IsHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        if(!$mail->Send()) {
+           echo 'Message could not be sent.';
+           echo 'Mailer Error: ' . $mail->ErrorInfo;
+           exit;
+        }
+
+    }
+
+    protected function sendTo(){
+
+        switch (self::send_to){
+            case 'new_relic':
+                self::sendToNewRelic();
+            break;
+
+            case 'zendesk':
+                self::sendToZendesk();
+            break;
+
+            default:
+                self::sendToEmail();
+        }
+
+    }
+
     static public function register(){
         if (!self::$registered){
             // saves old error reporting
@@ -33,6 +88,7 @@ class PhpErrorClass{
         }
 
     }
+
     static public function unregister(){
         if (self::$registered){
             // restore old configuration values
